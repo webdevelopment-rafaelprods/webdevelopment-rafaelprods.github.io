2bb0da444dbed73c3e9349245a191438b2aa5cb6
webdevelopment-rafaelprods <157218294+webdevelopment-rafaelprods@users.noreply.github.com>
2024-01-20 13:53:29 +0100

    Add files via upload

---
+++ js/100js/footer.js
@@ -0,0 +1,11 @@
+document.write(`
+
+<footer>
+
+        <p>&copy; 2024 &ndash; Rafaël Productions &ndash; All rights reserved.</p>
+        <p>With the exception of content belonging to AsmrProg or any expressly mentioned rights holder, the content of this site is the property of Rafaël Productions.</p>
+        <p>Rafaël Productions is not affiliated with AsmrProg.</p>
+
+    </footer>
+
+`);
---
+++ js/100js/header.js
@@ -0,0 +1,22 @@
+document.write(`
+
+<header>
+
+        <nav>
+            <div class="nav-logo">
+                <img src="/assets/media/100js/favicon.ico" alt="Icon">
+                <h1>100% JavaScript</h1>
+            </div>
+            <div class="nav-list">
+                <ul>
+                    <a href="#">Home</a>
+                    <a href="#about">About</a>
+                    <a href="#projects">Projects</a>
+                    <a href="/index.html">←</a>
+                </ul>
+            </div>
+        </nav>
+
+</header>
+
+`);
