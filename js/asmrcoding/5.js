7d6cf4d6e9843a0edb8a1b23149af313385be25f
webdevelopment-rafaelprods <157218294+webdevelopment-rafaelprods@users.noreply.github.com>
2024-01-28 07:13:11 +0100

    Add files via upload

---
+++ js/asmrcoding/5.js
@@ -0,0 +1,38 @@
+// console.log('Test');
+
+// var swiper = new Swiper('.swiper-container', {
+//     slidesPerView: 1,
+//     spaceBetween: 20,
+//     effect: 'fade',
+//     loop: true,
+//     speed: 300,
+//     mousewheel: {
+//         invert: false
+//     },
+//     pagination: {
+//         el: '.swiper-pagination',
+//         clickable: true,
+//         dynamicBullets: true
+//     },
+//     navigation
+// })
+
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    effect: 'fade',
+    loop: true,
+    speed: 300,
+    mousewheel: {
+        invert: false
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    }
+});
